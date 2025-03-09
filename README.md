c867894f66b3aaa2e7324b722c804a7718659e19
dominuszagare <ddominik33@gmail.com>
2025-03-09 01:05:57 +0100

    understanding midi protocol

--- README.md
+++ README.md
@@ -9,7 +9,54 @@
 ## seting up OSC sender in Resonite
 
 1. create OSC sender componnent
+
 1.1. set active user
+
 1.2. set the URL
+
 2. add OSC value component
 3. drive the value
+
+# MIDI protocol 
+There are EIGHT groups (families) of commands which are sent/received by a MIDI device, usually a synthesizer keyboard, sound module, computer, or other piece of hardware. In some instances, computer software will EMULATE a piece of MIDI hardware.
+
+    1. Note OFF
+    2. Note ON
+    3. Polyphonic key pressure
+    4. Control change
+    5. Program change
+    6. Monophonic key (channel) pressure
+    7. Pitch bend
+    8. System exclusive (usually GLOBAL commands affecting the entire device)
+
+A MIDI command consists of a series of numbers which when received by a device through a serial cable (or virtual connection made with software) will cause the device to do something, for example: play a note, change the sound (program), turn a note off, etc.
+
+Except for the SYSTEM EXCLUSIVE command (which can have any quantity of individual numbers from as few as 4 to as many as 1000's), all MIDI commands have either TWO or THREE numbers. For example:
+
+    144 60 127 - turn ON note #60 on MIDI channel 1 with a velocity of 127
+
+    144 60 0 - turn OFF note #60 on MIDI channel 1
+
+    192 15 - change the program (sound) on MIDI channel 1 to program #15
+
+    193 21 - change the program (sound) on MIDI channel 2 to program #21
+
+The FIRST number in any MIDI command is called the STATUS and is always in the range 128 - 255. The number(s) following the STATUS are DATA and are always in the range 0 - 127. 
+
+A MIDI byte (8 bits) can only represent a value from 0-127 since it uses the most significant bit to indicate whether it's a status byte or data byte
+
+Some MIDI commands have only ONE DATA number and some have TWO. Here is a list of the various MIDI COMMANDS along with their respective data requirements (the commands in italics will not be used in this class):
+
+![alt text](image.png)
+
+The MIDI specification allows for 16 MIDI channels and MIDI activity can take place on all 16 MIDI channels simultaneously. MIDI channels are identified by individual MIDI commands through the the RANGE of their STATUS numbers.
+
+Pitch Bend 14bit value (0–16383) is split between data1 and data2. data1 contains first 7 (LSB less significant bits) and data2 contains the last 7 (MSB most significant bits)
+
+The range of possible STATUS number is 128 - 255. Here is a list of the EIGHT MIDI command STATUS groups along with the range of numbers for the 16 MIDI channels in each group:
+
+![alt text](image-1.png)
+
+An easier way to deal with the complexity of keeping track of the STATUS number in regards to which MIDI channel it represents is to use HEXADECIMAL numbers instead of DECIMAL. For example, the #192 by itself does not intuitively indicate that that is the STATUS command for changing the program (sound) on MIDI channel 1. However if this table of HEXADECIMAL values was used for the STATUS numbers, it becomes immediately intuitive and easy to memorize:
+
+![alt text](image-2.png)
--- simpleOSCtoMIDI.py
+++ simpleOSCtoMIDI.py
@@ -3,9 +3,9 @@ from pythonosc.osc_server import BlockingOSCUDPServer
 import rtmidi
 
 midiout = rtmidi.MidiOut()
+midiin = rtmidi.MidiIn()
 available_ports = midiout.get_ports()
 
-DEBUG = True
 ip = "127.0.0.1"
 port = 4888
 
@@ -24,10 +24,25 @@ def parseValue(value):
             return 127
     return 0
 
+def parseValueLSB_MSB(value):
+    lsb = 0
+    msb = 0
+    try:
+        f = float(value)
+        val = int((f) * 16383)
+        lsb = val & 127
+        msb = val >> 7
+        return lsb, msb
+        
+    except ValueError:
+        if(value == "True"):
+            return 16383
+    return lsb, msb
+
 def print_handler(address, *args):
     print(f"{address}: {args}")
 
-# expected addres structure .../raw/<chanel>/<button>
+# expected addres structure .../note/<chanel>/<button>
 def midi_handler_note(address, *args):
     global midiout
     addresSplit = address.split("/")
@@ -43,19 +58,72 @@ def midi_handler_note(address, *args):
         print("invaliud button name", addresSplit[-1])
 
     for arg in args:
-        midiout.send_message([0x90 + channel, button, parseValue(arg)])
+        midiout.send_message([0x90 + channel, button, parseValue(arg)]) #sends midi message bytes in oreder [byte1,byte2,byte3]
 
-# expected addres structure .../cc/<cc>
+# expected addres structure .../cc/<chanel>/<cc>
 def midi_handler_CC(address, *args):
     global midiout
     addresSplit = address.split("/")
     cc = 0
+    channel = 0
+    try:
+        channel = int(addresSplit[-2])
+    except:
+        print("invaliud channel name", addresSplit[-2])
     try:
         cc = int(addresSplit[-1])
     except:
         print("invaliud cc name", addresSplit[-1])
     for arg in args:
-        midiout.send_message([0xB0, cc, parseValue(arg)])
+        midiout.send_message([0xB0 + channel, cc, parseValue(arg)])
+
+# expected addres structure /hex/status/ or /hex/status/data1/ or /hex/status/data1/data2
+def midi_handler_hex(address, *args):
+    global midiout
+    addresSplit = address.split("/")
+    status = 0
+    data1 = 0
+    data2 = 0
+    try:
+        status = int(addresSplit[2],16)
+    except:
+        print("invalid status", addresSplit[1])
+    if len(addresSplit) > 3:
+        try:
+            data1 = int(addresSplit[3],16)
+        except:
+            print("invalid data1", addresSplit[2])
+    if len(addresSplit) > 4:
+        try:
+            data2 = int(addresSplit[4],16)
+        except:
+            print("invalid data2", addresSplit[4])
+
+    
+    for arg in args:
+        print(address, status, data1, data2, parseValue(arg), parseValueLSB_MSB(arg))
+        if(status > 128 and status < 144):
+            #OFF velocity
+            midiout.send_message([status, data1, parseValue(arg)])
+        if(status > 159 and status < 176):
+            #ON velocity
+            midiout.send_message([status, data1, parseValue(arg)])
+        if(status > 143 and status < 160):
+            #ploy key presure
+            midiout.send_message([status, data1, parseValue(arg)])
+        if(status > 175 and status < 192):
+            #control change
+            midiout.send_message([status, data1, parseValue(arg)])
+        if(status > 191 and status < 208):
+            #program chnage
+            midiout.send_message([status, data1])
+        if(status > 207 and status < 224):
+            #chanel presure
+            midiout.send_message([status, parseValue(arg)])
+        if(status > 223 and status < 240):
+            #pitch bend
+            lsb, msb = parseValueLSB_MSB(arg)
+            midiout.send_message([status,lsb,msb]) 
 
 def update_midi_CCTap(cc):
     global midiout
@@ -66,11 +134,10 @@ print("OSC server running on ", f'osc://{ip}:{port}')
 
 
 dispatcher = Dispatcher()
-if DEBUG:
-    dispatcher.map("/*", print_handler) #print
 
 dispatcher.map("/cc/*", midi_handler_CC)
-dispatcher.map("/raw/*", midi_handler_note)
+dispatcher.map("/note/*", midi_handler_note)
+dispatcher.map("/hex/*", midi_handler_hex)
 
 server = BlockingOSCUDPServer((ip, port), dispatcher)
 server.serve_forever()  # Blocks forever
